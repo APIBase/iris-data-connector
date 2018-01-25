fb85774033e422937dc249edf5b62d7936f31315
Freehaha <freehaha@gmail.com>
2018-01-25 17:02:27 +1100

    implement

--- test.js
+++ test.js
@@ -1,14 +1,5 @@
 var connector = require('./index.js');
 
-var SOURCE = {
-  q: "select * from geo.places where text='sunnyvale'"
-};
-var TARGET_RESOURCE_TYPE = "";
-
-connector(SOURCE, TARGET_RESOURCE_TYPE, function(err, result) {
-  if (err) {
-    console.error(err);
-    return;
-  }
+connector({}, null, function(err, result) {
   console.log(result);
 });
